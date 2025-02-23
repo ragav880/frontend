13851fbde8efeb040232841592d4974e44e6028c
ragava <raghavareddypathi@gmail.com>
2025-02-23 10:56:32 +0530

    home page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-    <form>
-      <label>
-        Username:
-        <input type="text" name="username" />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" name="password" />
-      </label>
-      <br />
-      <button type="submit">Login</button>
-    </form>
+    
       
       
     </div>
